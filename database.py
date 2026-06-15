cc17009af0a0f86229568ab15eca9b01ee8eac5b
Anthony Santos <anthonysantos180108@gmail.com>
2026-06-15 19:26:36 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,19 @@
 import mysql.connector
 
+
 def get_connection():
-    return mysql.connector.connect(
-        host="mysql-594d6c0-mini-tarefas.h.aivencloud.com",
-        port=17669,
-        user="avnadmin",
-        password="SUA_SENHA_DO_AIVEN",
-        database="defaultdb",
-        ssl_disabled=False
-    )
+    try:
+        conn = mysql.connector.connect(
+            host="mysql-594d6c0-mini-tarefas.h.aivencloud.com",
+            port=17669,
+            user="avnadmin",
+            password="COLE_AQUI_A_SENHA_DO_AIVEN",
+            database="defaultdb",
+            ssl_disabled=False
+        )
+
+        return conn
+
+    except mysql.connector.Error as err:
+        print(f"Erro ao conectar ao banco: {err}")
+        return None
