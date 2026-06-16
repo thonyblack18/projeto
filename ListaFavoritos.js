5353ab525ea9c5e6dbb21ed3f005fddefbca138d
Anthony Santos <anthonysantos180108@gmail.com>
2026-06-16 20:47:29 -0300

    Atualizar o ListaFavoritos.js

--- ListaFavoritos.js
+++ ListaFavoritos.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:5000";
+const API_BASE = "https://projeto-w9ao.onrender.com";
 
 // =================== ESTADO ===================
 let favoritosIds = JSON.parse(localStorage.getItem("velora_favoritos_ids") || "[]");
