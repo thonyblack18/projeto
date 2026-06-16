af9937559d30ee6fb77902954fe11d601a1c11df
Anthony Santos <anthonysantos180108@gmail.com>
2026-06-15 22:07:54 -0300

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:5000";
+const API_BASE = "https://projeto-w9ao.onrender.com";
 
 const params = new URLSearchParams(window.location.search);
 const gameId = params.get("id");
